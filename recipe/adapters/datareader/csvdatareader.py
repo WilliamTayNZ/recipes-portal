4ea094dd5387ca08417aefcd7b71370991195b73
aleckshen <aleckshn@gmail.com>
2025-08-20 11:31:26 +1200

    Created read function and tested functionality by printing each row

--- recipe/adapters/datareader/csvdatareader.py
+++ recipe/adapters/datareader/csvdatareader.py
@@ -5,7 +5,18 @@ from recipe.domainmodel.category import Category
 from recipe.domainmodel.nutrition import Nutrition
 from recipe.domainmodel.recipe import Recipe
 
-
 class CSVDataReader:
-    # TODO: Complete the implementation of the CSVDataReader class.
-    pass
+    def __init__(self, csv_path):
+        self.__csv_path = csv_path
+        self.__recipes = []
+
+    def csv_read(self):
+        with open(self.__csv_path, mode='r', newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                print(row)
+
+if __name__ == '__main__':
+    csv_path = "./../data/recipes.csv"
+    reader = CSVDataReader(csv_path)
+    reader.csv_read()
